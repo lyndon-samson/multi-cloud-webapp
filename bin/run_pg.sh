4935cd3bccf7e97741ac10008dff576fd6c81ed6
Lyndon Samson <lyndon.samson@gmail.com>
2024-01-11 12:04:16 +0000

    updates to scripts

--- bin/run_pg.sh
+++ bin/run_pg.sh
@@ -1,2 +1,4 @@
 DATA_DIR=/home/admin/data
+DATA_DIR=/home/samsonl/data
+DATA_DIR=$HOME/data
 docker run --rm --network host -v $DATA_DIR:/var/lib/postgresql/data --name postgres -e 'POSTGRES_PASSWORD' -d postgres:14.5
--- bin/run_pg_import.sh
+++ bin/run_pg_import.sh
@@ -1,2 +1,4 @@
 DATA_SQL_DIR=/home/admin/multi-cloud-webapp/data
+DATA_SQL_DIR=/home/samsonl/multi-cloud-webapp/data
+DATA_SQL_DIR=$HOME/multi-cloud-webapp/data
 docker run -it -v$DATA_SQL_DIR:/data  --rm --network host postgres:14.5 psql -h localhost -U postgres -f /data/quotes.sql
